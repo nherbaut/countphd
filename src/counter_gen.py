fcb0b97fee9f75cfd3c84f25deaac91ae8daf806
Nicolas Herbaut <nherbaut@labri.fr>
2017-06-04 07:28:06 +0200

    counter ok

--- src/counter_gen.py
+++ src/counter_gen.py
@@ -2,6 +2,10 @@
 import argparse
 import subprocess
 
+import os
+import yaml
+import tempfile
+from git import Repo
 from jsmin import jsmin
 from jinja2 import Environment, PackageLoader, select_autoescape
 from PyPDF2 import PdfFileReader
@@ -9,7 +13,7 @@ from PyPDF2 import PdfFileReader
 
 parser = argparse.ArgumentParser(description='Generate js counter of a pdf document')
 
-parser.add_argument('--file_in', default="thesis.pdf",)
+parser.add_argument('--config', default="config.yaml",)
 parser.add_argument('--file_out', default="thesis_counter.js",)
 parser.add_argument('--delay', default=5, type=int)
 
@@ -20,12 +24,28 @@ env = Environment(
 )
 
 template = env.get_template('thesis_counter.js.tpl')
-with open(args.file_in, "rb") as f:
+
+#get the config file
+with open(args.config) as f:
+    config=yaml.load(f.read())
+
+
+#clone the repo
+directory=tempfile.TemporaryDirectory()
+Repo.clone_from(config["phd"]["url"], directory.name)
+output=subprocess.call(["latexmk","-pdf",config["phd"]["file"]],cwd=directory.name)
+if output != 0:
+    exit(-1)
+
+thesis_file_name=os.path.join(directory.name,config["phd"]["file"]).replace("tex","pdf")
+
+
+with open(thesis_file_name, "rb") as f:
     thesis_file = PdfFileReader(f)
     pages_count=thesis_file.getNumPages()
 
 
-words_count=int(subprocess.check_output('pdftotext %s  -|wc -w'%args.file_in,
+words_count=int(subprocess.check_output('pdftotext %s  -|wc -w'%thesis_file_name,
                         shell=True))
 
 
